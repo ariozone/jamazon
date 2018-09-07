126c4a23c7df53923475b7f407fd5d5d53274024
Ario Tamaddoni <ariozone6@gmail.com>
2018-09-07 10:02:48 -0700

    Commented code removed #1

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ var app = {
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         origin: 'Turkey',
         imageUrl: 'https://www.mydukkan.com/media/image/33108/Vatan.Vatan3025K33108.jpg'
-        // 'https://goo.gl/Q7X6VB'
       },
       {
         itemId: 3,
